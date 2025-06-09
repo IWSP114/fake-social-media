105b4a5ac5c27e1ebf1448f085b4f7fc968fd8e8
IWSP <moneytsui.mt@gmail.com>
2025-06-09 22:09:01 +0800

    Refactor code structure for improved readability and maintainability

--- app/(pages)/friends/leftSideBar.tsx
+++ app/(pages)/friends/leftSideBar.tsx
@@ -1,15 +1,9 @@
 "use client"
 
-import OptionsIcon from "../../../assest/FriendsPage/options-icon.png";
-import FriendsIcon from "../../../assest/FriendsPage/friends-icon.png";
-import AllFriendsIcon from "../../../assest/FriendsPage/all-friends-icon.png";
-import FriendsSuggestIcon from "../../../assest/FriendsPage/friends-suggest-icon.png";
-import FriendsInviteIcon from "../../../assest/FriendsPage/friends-invite-icon.png";
-import GiftIcon from "../../../assest/FriendsPage/gift-icon.png";
-import Image from "next/image"
 import { FriendOptions } from "./friendOptions";
 import { useState } from "react";
 import Link from "next/link";
+import { BookUser, Gift, Settings, UserCheck, UserPlus, UserRoundCog, Users } from "lucide-react";
 
 
 export function LeftSideBar() {
@@ -20,12 +14,9 @@ export function LeftSideBar() {
       <div className="flex flex-col">
         <div className="relative flex justify-between items-center px-2">
           <h1 className="my-2 font-bold font-sans text-2xl">Friends</h1>
-          <button onClick={() => setShowFriendOptions((s => !s))} className="relative rounded-full bg-slate-200 hover:bg-slate-300 active:scale-98 w-8 h-8 p-2 cursor-pointer">
-            <Image
-              src={OptionsIcon}
-              alt="User Icon"
-              className="object-cover"
-            />
+          <button onClick={() => setShowFriendOptions((s => !s))} className={`relative rounded-full ${showFriendOptions ? "bg-sky-300" : "bg-slate-200" } hover:bg-gray-400 active:scale-98 w-8 h-8 p-2 cursor-pointer`}>
+            {showFriendOptions && <Settings color="#ffffff" strokeWidth={3} className="w-full h-full text-gray-700" />}
+            {!showFriendOptions && <Settings strokeWidth={3} className="w-full h-full text-gray-700" />}
           </button>
           {/* Friends option */}
             <FriendOptions visable={showFriendOptions} />
@@ -33,105 +24,63 @@ export function LeftSideBar() {
         </div>
 
         <Link href="/">
-        <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
-          <div className="rounded-full w-10 h-10 p-2 bg-slate-200">
-            <Image
-              src={FriendsIcon}
-              alt="User Icon"
-              style={{
-                width: 'auto',
-                height: '100%',
-                objectFit: 'contain'
-              }} />
-          </div>
+          <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
+            <div className="rounded-full w-10 h-10 p-2 bg-slate-200">
+              <Users className="w-full h-full text-gray-700" />
+            </div>
 
-          <h3 className="mx-2 font-bold">Main Page</h3>
-        </div>
+            <h3 className="mx-2 font-bold">Main Page</h3>
+          </div>
         </Link>
 
         <Link href="/">
-        <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
-          <div className="rounded-full w-10 h-10 p-2 bg-slate-200">
-            <Image
-              src={FriendsSuggestIcon}
-              alt="User Icon"
-              style={{
-                width: 'auto',
-                height: '100%',
-                objectFit: 'contain'
-              }} />
-          </div>
+          <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
+            <div className="rounded-full w-10 h-10 p-2 bg-slate-200">
+              <UserPlus className="w-full h-full text-gray-700" />
+            </div>
 
-          <h3 className="mx-2 font-bold">Friends Suggestion</h3>
-        </div>
+            <h3 className="mx-2 font-bold">Friends Suggestion</h3>
+          </div>
         </Link>
 
         <Link href="/">
-        <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
-          <div className="rounded-full w-10 h-10 p-2 bg-slate-200">
-            <Image
-              src={FriendsInviteIcon}
-              alt="User Icon"
-              style={{
-                width: 'auto',
-                height: '100%',
-                objectFit: 'contain'
-              }} />
-          </div>
+          <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
+            <div className="rounded-full w-10 h-10 p-2 bg-slate-200">
+              <UserCheck className="w-full h-full text-gray-700" />
+            </div>
 
-          <h3 className="mx-2 font-bold">Friend Invitation</h3>
-        </div>
+            <h3 className="mx-2 font-bold">Friend Invitation</h3>
+          </div>
         </Link>
 
         <Link href="/">
-        <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
+          <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
           <div className="rounded-full w-10 h-10 p-2 bg-slate-200">
-            <Image
-              src={AllFriendsIcon}
-              alt="User Icon"
-              style={{
-                width: 'auto',
-                height: '100%',
-                objectFit: 'contain'
-              }} />
+            <BookUser className="w-full h-full text-gray-700" />
           </div>
 
           <h3 className="mx-2 font-bold">All Friends</h3>
-        </div>
+          </div>
         </Link>
 
         <Link href="/">
-        <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
+          <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
           <div className="rounded-full w-10 h-10 p-2 bg-slate-200">
-            <Image
-              src={GiftIcon}
-              alt="User Icon"
-              style={{
-                width: 'auto',
-                height: '100%',
-                objectFit: 'contain'
-              }} />
+            <Gift className="w-full h-full text-gray-700" />
           </div>
 
           <h3 className="mx-2 font-bold">Birthday</h3>
-        </div>
+          </div>
         </Link>
 
         <Link href="/">
-        <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
+          <div className="flex items-center hover:bg-gray-300 px-2 py-2 rounded-lg cursor-pointer">
           <div className="rounded-full w-10 h-10 p-2 bg-slate-200">
-            <Image
-              src={AllFriendsIcon}
-              alt="User Icon"
-              style={{
-                width: 'auto',
-                height: '100%',
-                objectFit: 'contain'
-              }} />
+           <UserRoundCog className="w-full h-full text-gray-700" />
           </div>
 
           <h3 className="mx-2 font-bold">Customize</h3>
-        </div>
+          </div>
         </Link>
 
       </div>
